2f18630605c7a6df00bc823b736a147549e99b43
Christian Persson <saser@live.se>
2016-04-19 16:18:03 +0200

    Add a little documentation

--- src/main/java/me/saser/libinftom/StringLanguage.java
+++ src/main/java/me/saser/libinftom/StringLanguage.java
@@ -8,6 +8,11 @@ public class StringLanguage implements Language {
 
     private final Set<String> symbolSet;
 
+    /**
+     * Create a language from the given <b>comma separated string</b> of symbols. Be sure to not include commas in the
+     * symbols themselves.
+     * @param symbols a comma separated string of symbols
+     */
     public StringLanguage(String symbols) {
         this.symbolSet = ImmutableSet.copyOf(symbols.split(","));
     }
