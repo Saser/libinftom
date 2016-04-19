3b76dfbfc18e23c3ae74ec3d23c84c255b23284d
Christian Persson <saser@live.se>
2016-04-19 16:43:30 +0200

    Test for isValidWord

--- src/test/java/me/saser/libinftom/LanguageTest.java
+++ src/test/java/me/saser/libinftom/LanguageTest.java
@@ -36,4 +36,17 @@ public class LanguageTest {
         assertFalse("'hello, world' is not a valid symbol (contains a comma)", language.isValidSymbol("hello, world"));
     }
 
+    @Test
+    public void isValidWord() throws Exception {
+        assertTrue("'' (the empty word) is a valid word", language.isValidWord(""));
+        assertTrue("'a', a word with a single symbol, is a valid word", language.isValidWord("a"));
+        assertTrue("'hello world', a word with a single symbol, is a valid word", language.isValidWord("hello world"));
+        assertTrue("'a,b,cde', a word with three symbols, is a valid word", language.isValidWord("a,b,cde"));
+        assertTrue("'a,a,hello world,a,b', a word with a few symbols, is a valid word", language.isValidWord("a,a,hello world,a,b"));
+
+        assertFalse("'q', a word with a single invalid symbol, is an invalid word", language.isValidWord("q"));
+        assertFalse("'a,q', a word with one valid and one invalid symbol, is an invalid word", language.isValidWord("a,q"));
+        assertFalse("'a,cde,hello world,q', a word with a few valid symbols and one invalid symbol, is an invalid word", language.isValidWord("a,cde,hello world,q"));
+        assertFalse("'q,q,q,derp,sc2', a word with a few invalid symbols, is an invalid word", language.isValidWord("q,q,q,derp,sc2"));
+    }
 }
