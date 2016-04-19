1dc6ed43b2cc24cad6d0907acbac6904397a45e8
Christian Persson <saser@live.se>
2016-04-19 16:33:00 +0200

    A few more wording updates

--- src/main/java/me/saser/libinftom/Language.java
+++ src/main/java/me/saser/libinftom/Language.java
@@ -11,15 +11,15 @@ public interface Language {
     Set<String> getSymbols();
 
     /**
-     * Determines whether the given symbol is a symbol in this language.
-     * @param symbol
+     * Determines whether the given String is a symbol in this language.
+     * @param symbol a String for a single symbol
      * @return true if the symbols is valid
      */
     Boolean isValidSymbol(String symbol);
 
     /**
      * Determines whether the given word is a word in this language. The word is expected to be a <b>comma separated
-     * string</b> of symbols. A word is valid if all symbols in it are valid symbols in this language.
+     * String</b> of symbols. A word is valid if all symbols in it are valid symbols in this language.
      * @param word a comma separated list of symbols in this language
      * @return true if all symbols are valid
      */
